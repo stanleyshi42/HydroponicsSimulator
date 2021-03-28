2881da79bc7be7c4e95cc68ae19acdbdc0b121f5
Stanley Shi <stanleyshi42@gmail.com>
2021-03-28 03:18:13 -0700

    Add files via upload

---
+++ src/hydroponicssimulator/HydroponicsSimulator.java
@@ -0,0 +1,12 @@
+// Stanley Shi
+// 3/27/21
+// TuffyHacks 2021
+package hydroponicssimulator;
+
+public class HydroponicsSimulator {
+    public static void main(String[] args) {
+        HydroponicsSimulatorUI frame = new HydroponicsSimulatorUI();
+        frame.setVisible(true);
+        frame.startTimer();
+    } 
+}
